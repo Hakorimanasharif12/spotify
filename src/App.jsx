922cf7fdaac37462021be7457c590e5afc0acab1
developer <developer@gmail.com>
2024-07-20 22:30:22 +0200

    Add album and song list

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,19 @@
-import React from 'react'
+import React, { useContext } from 'react'
+import Sidebar from './components/Sidebar'
+import Player from './components/Player'
+import Display from './components/Display'
+import { PlayerContext } from './context/PlayerContext'
 
 const App = () => {
+  const {audioRef, track, } = useContext(PlayerContext)
   return (
-    <div>
-      <h1 className="text-3xl font-bold underline">
-      Hello world!
-      </h1>
+    <div className='h-screen bg-black'>
+     <div className='h-[90%] flex'>
+      <Sidebar />
+      <Display />
+     </div>
+     <Player />
+     <audio ref={audioRef} src={track.file} preload='auto'></audio>
     </div>
   )
 }
